c4e8e28569722575cba128388b300ef5051d4af4
Albert Garuda <algarud@gmail.com>
2022-04-05 23:53:03 +0300

    wave execution

--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,28 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+
     const waveContractFactory = await hre.ethers.getContractFactory("Waver");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
-  };
-  
-  const runMain = async () => {
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
+        await main();
+        process.exit(0);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+        console.log(error);
+        process.exit(1);
     }
-  };
-  
-  runMain();
+};
+
+runMain();
