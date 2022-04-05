18978ec16a75cc570834c3b176e611b3a329ff16
Albert Garuda <algarud@gmail.com>
2022-04-05 23:51:52 +0300

    contract to wave

--- contracts/Waver.sol
+++ contracts/Waver.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Waver {
+
+    uint256 totalWaves;
+    
     constructor() {
         console.log("wave constructor");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
