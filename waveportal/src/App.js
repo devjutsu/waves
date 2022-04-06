c7c7caba40291674bf100c0974134ac4f69f29b1
Garuda <algarud@gmail.com>
2022-04-06 23:35:06 +0300

    connect wallet

--- waveportal/src/App.js
+++ waveportal/src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  /*
-  * Just a state variable we use to store our user's public wallet.
-  */
   const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
@@ -23,12 +20,33 @@ const App = () => {
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
+        setCurrentAccount(account);
       } else {
         console.log("No authorized account found")
       }
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -40,18 +58,28 @@ const App = () => {
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-         Greetings, people of the World!
+        Greetings, people of the World!
         </div>
 
         <div className="bio">
-          DevJutsu here 🥷
+        devjutsu here &#128122;
         </div>
 
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
-    );
-  }
+  );
+}
+
 export default App
